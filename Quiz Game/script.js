229128ffa377e1b1c9c3e8ca18f5e3b849bd9ae2
Michael T. Hope <140920291+Mikehope254@users.noreply.github.com>
2024-05-28 09:35:34 +0300

    Update script.js

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -6,7 +6,7 @@ console.log("Welcome to the computer hardware quiz")
 let correctAnswers = 0
 const totalQuestions = 3
 
-const answer1 = prompt("What is the brain of the computer? ")
+const question1 = prompt("What is the brain of the computer? ")
 const correctAnswer1 = "CPU"
 
 if (answer1.toUpperCase() === correctAnswer1){ //.toUpperCase() method converts lowercase input to Uppercase for optionality
@@ -16,7 +16,7 @@ if (answer1.toUpperCase() === correctAnswer1){ //.toUpperCase() method converts 
     console.log("You got it wrong")
 }
 
-const answer2 = prompt("What is better 3090ti or 4060? ")
+const question2 = prompt("What is better 3090ti or 4060? ")
 const correctAnswer2 = "3090ti"
 
 if (answer2.toLowerCase() === correctAnswer2){
@@ -26,7 +26,7 @@ if (answer2.toLowerCase() === correctAnswer2){
     console.log("You got it wrong")
 }
 
-const answer3 = prompt("What is the recommended amount of RAM in 2024? ")
+const question3 = prompt("What is the recommended amount of RAM in 2024? ")
 const correctAnswer3 = "16GB"
 
 if (answer3.toUpperCase() === correctAnswer3){
